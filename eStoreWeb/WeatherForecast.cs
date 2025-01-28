b83831ed62884a5143fc829822930899b3dcb747
Uladzislau Baryshchyk <Uladzislau.Baryshchyk@osgconnect.com>
2025-01-28 08:15:01 -0600

    removed unused property

--- eStoreWeb/WeatherForecast.cs
+++ eStoreWeb/WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace eStoreAPI
     public class WeatherForecast
     {
         public DateOnly Date { get; set; }
-        public int MyProperty { get; set; }
+        
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
