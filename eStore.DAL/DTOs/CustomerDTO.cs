536714f253ac86add7c86ce95131b95f06a5969d
Uladzislau Baryshchyk <Uladzislau.Baryshchyk@osgconnect.com>
2025-01-30 13:25:35 -0600

    added models

---
+++ eStore.DAL/DTOs/CustomerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore.BL.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
